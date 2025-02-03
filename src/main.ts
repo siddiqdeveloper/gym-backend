610e46577ab2490ea3c63f1ce4e53be791aae348
siddiqdeveloper <asiddiq538@gmail.com>
2025-02-03 12:24:44 +0530

    merge code

--- src/main.ts
+++ src/main.ts
@@ -2,19 +2,47 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { json, urlencoded } from 'express';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
+import * as dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const PORT = process.env.PORT || 3001; // Default to 3000 if not set in .env
+  const USE_SSL = process.env.USE_SSL === 'true'; // Check if SSL is enabled
 
-  app.enableCors();
-    app.setGlobalPrefix('api/v1');
+  let app;
+
+  // If SSL is enabled, configure HTTPS options
+  if (USE_SSL) {
+    const httpsOptions = {
+      key: fs.readFileSync('/etc/letsencrypt/live/backend.gymitfitness.com/privkey.pem'),
+      cert: fs.readFileSync('/etc/letsencrypt/live/backend.gymitfitness.com/fullchain.pem'),
+    };
+
+    app = await NestFactory.create(AppModule, { httpsOptions });
+    console.log(`Server running on HTTPS at https://backend.gymitfitness.com:${PORT}`);
+  } else {
+    app = await NestFactory.create(AppModule);
+    console.log(`Server running on HTTP at http://backend.gymitfitness.com:${PORT}`);
+  }
+
+  // Enable global CORS
   app.enableCors({ credentials: true, origin: '*', allowedHeaders: '*' });
-  app.useGlobalPipes(new ValidationPipe({ whitelist: false }));
- 
+  
+  // Set global API prefix
+  app.setGlobalPrefix('api/v1');
+
+  // Apply global validation pipe
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Set up body parsers with a larger limit for JSON and URL encoded data
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
 
-  await app.listen(process.env.port);
+  // Start the application
+  await app.listen(PORT);
 }
 
 bootstrap();
