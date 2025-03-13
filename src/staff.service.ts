8b21731696bbf699328724ca0456fba71be9372e
siddiqdeveloper <asiddiq538@gmail.com>
2025-03-13 19:10:03 +0530

    mereg code

--- src/staff.service.ts
+++ src/staff.service.ts
@@ -26,10 +26,31 @@ export class StaffService {
       if (files.otherDocuments) {
         staff.otherDocuments = files.otherDocuments.map(file => file.path); // Map multiple file paths for otherDocuments
       }
+
+      if (files.dateOfBirth) {
+        staff.dateOfBirth = this.formatDateForMySQL(staff.dateOfBirth);
+      }
+
+      if (files.dateOfJoining) {
+        staff.dateOfJoining  = this.formatDateForMySQL(staff.dateOfJoining); 
+      }
     }
 
     return await this.staffRepository.save(staff);
   }
+
+  formatDateForMySQL(date) {
+    const d = new Date(date);
+    const year = d.getFullYear();
+    const month = String(d.getMonth() + 1).padStart(2, '0');
+    const day = String(d.getDate()).padStart(2, '0');
+    const hours = String(d.getHours()).padStart(2, '0');
+    const minutes = String(d.getMinutes()).padStart(2, '0');
+    const seconds = String(d.getSeconds()).padStart(2, '0');
+
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+  }
+
 
 
 
